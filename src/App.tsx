df1ddce1efe441f06b61b7d3eaa7ef2a0c905acb
Prabakaran Ravichandran <prabakaran.r@hyperready.tech>
2025-03-05 18:45:25 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <button className="px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
+        Click me
+      </button>
+    </div>
+  );
+}
+
+export default App;
