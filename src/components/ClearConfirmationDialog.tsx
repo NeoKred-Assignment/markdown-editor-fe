a9066fc4e0d7b0a69585ab8ad167d07417681fe9
Prabakaran Ravichandran <prabakaran.r@hyperready.tech>
2025-03-08 12:26:16 +0530

    fix(file-upload): file upload enhancement

--- src/components/ClearConfirmationDialog.tsx
+++ src/components/ClearConfirmationDialog.tsx
@@ -12,7 +12,7 @@ import { Button } from "../components/ui/button";
 
 interface ClearConfirmationDialogProps {
   open: boolean;
-  onOpenChange: () => void;
+  onOpenChange: (open: boolean) => void;
   onConfirm: () => void;
 }
 
